737508ee91145fd2da2d02115ffca07024826413
df6ih <guenther.juergen@gmail.com>
2026-07-20 16:31:12 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,31 +1,16 @@
-
 <?php
+/**
+ * GreenScape Pro
+ *
+ * Bootstrap file
+ *
+ * @package GreenScapePro
+ */
 
-if (!defined('ABSPATH')) {
-    exit;
-}
+defined('ABSPATH') || exit;
 
-function greenscape_setup() {
+define('GREENSCAPE_VERSION', '0.1.0');
 
-    add_theme_support('wp-block-styles');
-    add_theme_support('responsive-embeds');
-    add_theme_support('editor-styles');
-    add_theme_support('align-wide');
-
-}
-
-add_action('after_setup_theme', 'greenscape_setup');
-
-
-function greenscape_styles() {
-
-    wp_enqueue_style(
-        'greenscape-style',
-        get_stylesheet_uri(),
-        [],
-        wp_get_theme()->get('Version')
-    );
-
-}
-
-add_action('wp_enqueue_scripts', 'greenscape_styles');
+require_once get_theme_file_path('/inc/setup.php');
+require_once get_theme_file_path('/inc/enqueue.php');
+require_once get_theme_file_path('/inc/helpers.php');
