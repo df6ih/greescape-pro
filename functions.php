1ab8e6dfe599160a32212ae3fc8957d15d47da16
df6ih <guenther.juergen@gmail.com>
2026-07-20 11:58:28 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,1 +1,31 @@
 
+<?php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+function greenscape_setup() {
+
+    add_theme_support('wp-block-styles');
+    add_theme_support('responsive-embeds');
+    add_theme_support('editor-styles');
+    add_theme_support('align-wide');
+
+}
+
+add_action('after_setup_theme', 'greenscape_setup');
+
+
+function greenscape_styles() {
+
+    wp_enqueue_style(
+        'greenscape-style',
+        get_stylesheet_uri(),
+        [],
+        wp_get_theme()->get('Version')
+    );
+
+}
+
+add_action('wp_enqueue_scripts', 'greenscape_styles');
