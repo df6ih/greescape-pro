801d033952933aa35b57732908a444e0c13e98fa
df6ih <guenther.juergen@gmail.com>
2026-07-22 13:44:21 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -28,3 +28,24 @@ function greenscape_styles() {
 }
 
 add_action('wp_enqueue_scripts', 'greenscape_styles');
+
+
+
+
+
+<?php
+/**
+ * GreenScape Pro
+ *
+ * Bootstrap file
+ *
+ * @package GreenScapePro
+ */
+
+defined('ABSPATH') || exit;
+
+define('GREENSCAPE_VERSION', '0.1.0');
+
+require_once get_theme_file_path('/inc/setup.php');
+require_once get_theme_file_path('/inc/enqueue.php');
+require_once get_theme_file_path('/inc/helpers.php');
